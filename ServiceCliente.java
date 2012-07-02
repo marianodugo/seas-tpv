38e117f2ca41a0fc0450449f4029710c5fe731ad
sancho.ruben@gmail.com <sancho.ruben@gmail.com@06b723dc-65d5-93aa-df9f-f07fd2d1c42e>
2012-07-02 15:38:43 +0000

    .

---
+++ ServiceCliente.java
@@ -0,0 +1,31 @@
+package Clientes;
+
+/**
+ * Gestión de Clientes
+ * 
+ * @author Rubén Sancho <sancho.ruben@gmail.com>
+ * @version 1.1
+ * 
+ */
+public class ServiceCliente {
+    
+    public ServiceCliente(){}
+    
+    private void saveCliente(){
+        
+    }
+    
+    private void deleteCliente(String nif){
+        
+    }
+    
+    private void updateCliente(){
+        
+    }
+    
+    private Cliente getCliente(){
+        Cliente cli = new Cliente();
+        
+        return cli;
+    }
+}
