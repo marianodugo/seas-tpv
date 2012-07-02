14656547babcb0ccf7378186262589c15471c169
sancho.ruben@gmail.com <sancho.ruben@gmail.com@06b723dc-65d5-93aa-df9f-f07fd2d1c42e>
2012-07-02 15:37:36 +0000

    Subida inicial

---
+++ Cliente.java
@@ -0,0 +1,23 @@
+package Clientes;
+
+/**
+ * Gestión de Clientes
+ * 
+ * @author Rubén Sancho <sancho.ruben@gmail.com>
+ * @version 1.1
+ * 
+ */
+public class Cliente {
+    
+    int id;
+    String nif;
+    String nombre;
+    String apellidos;
+    String telefono;
+    String fecha_alta;
+    int descuento;
+    
+    public Cliente(){
+        
+    }
+}
